d81085d360ab71df4734ecdef5532bb0c4b134f5
webdevbiv <80968651+webdevbiv@users.noreply.github.com>
2023-07-04 22:38:57 +0300

    done

--- app.js
+++ app.js
@@ -7,3 +7,76 @@ function add(num1, num2) {
 button === null || button === void 0 ? void 0 : button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+var num;
+num = 10;
+var str;
+str = "Hello";
+var arr = [];
+arr = ["string"];
+var arrNumber = [];
+arrNumber = [1, 2, 3];
+var arrAny = [];
+arrAny = [1, 2, 3, {}, "string"];
+var arrObj = [];
+arrObj = [{ name: "string" }, { name: "nikita" }];
+var obj;
+obj = {
+    name: "Alex",
+};
+var db;
+db = {
+    id: 1,
+    title: "string",
+    info: {
+        date: new Date(),
+    },
+};
+var some;
+some = "string";
+some = 1;
+var numNew;
+numNew = some;
+var undef;
+undef = 10;
+undef = "string";
+var text;
+if (typeof undef === "string") {
+    text = undef;
+}
+var fixed;
+fixed = ["string", 1];
+var toggle;
+(function (toggle) {
+    toggle[toggle["ENABLE"] = 0] = "ENABLE";
+    toggle[toggle["DISABLE"] = 1] = "DISABLE";
+})(toggle || (toggle = {}));
+var service = { status: toggle.ENABLE };
+if (service.status === toggle.ENABLE) {
+    console.log("it is active");
+}
+var union;
+function combine(param1, param2) {
+    if (typeof param1 === "string" || typeof param2 === "string") {
+        return param1.toString() + param2.toString();
+    }
+    else {
+        return param1 + param2;
+    }
+}
+console.log(combine(1, 1));
+var active;
+var fruit = [];
+function workWithArr(arr, value, action) {
+    if (action === "add") {
+        arr.push(value);
+    }
+    else {
+        var index = arr.indexOf(value);
+        arr.splice(index, 1);
+    }
+    return arr;
+}
+workWithArr(fruit, "apple", "add");
+workWithArr(fruit, "banana", "add");
+workWithArr(fruit, "watermelon", "add");
+console.log(fruit);
--- app.ts
+++ app.ts
@@ -9,3 +9,119 @@ function add(num1: number, num2: number) {
 button?.addEventListener("click", () => {
   console.log(add(+input1.value, +input2.value));
 });
+
+let num: number;
+
+num = 10;
+
+let str: string;
+
+str = "Hello";
+
+let arr: string[] = [];
+
+arr = ["string"];
+
+let arrNumber: number[] = [];
+
+arrNumber = [1, 2, 3];
+
+let arrAny: any[] = [];
+
+arrAny = [1, 2, 3, {}, "string"];
+
+let arrObj: { name: string }[] = [];
+
+arrObj = [{ name: "string" }, { name: "nikita" }];
+
+let obj: { name: string };
+
+obj = {
+  name: "Alex",
+};
+
+let db: {
+  id: number;
+  title: string;
+  info?: {
+    date: Date;
+    isNew?: boolean;
+  };
+};
+
+db = {
+  id: 1,
+  title: "string",
+  info: {
+    date: new Date(),
+  },
+};
+
+let some: any;
+
+some = "string";
+
+some = 1;
+
+let numNew: number;
+
+numNew = some;
+
+let undef: unknown;
+
+undef = 10;
+
+undef = "string";
+
+let text: string;
+
+if (typeof undef === "string") {
+  text = undef;
+}
+
+let fixed: [string, number];
+
+fixed = ["string", 1];
+
+enum toggle {
+  ENABLE,
+  DISABLE,
+}
+
+const service = { status: toggle.ENABLE };
+
+if (service.status === toggle.ENABLE) {
+  console.log("it is active");
+}
+
+let union: number | string;
+
+function combine(param1: string | number, param2: string | number) {
+  if (typeof param1 === "string" || typeof param2 === "string") {
+    return param1.toString() + param2.toString();
+  } else {
+    return param1 + param2;
+  }
+}
+
+console.log(combine(1, 1));
+
+let active: "start" | "end";
+
+const fruit = [];
+
+function workWithArr(arr, value: string, action: "add" | "delete") {
+  if (action === "add") {
+    arr.push(value);
+  } else {
+    const index = arr.indexOf(value);
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+workWithArr(fruit, "apple", "add");
+workWithArr(fruit, "banana", "add");
+workWithArr(fruit, "watermelon", "add");
+
+console.log(fruit);
